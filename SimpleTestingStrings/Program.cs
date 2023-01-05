425f076cac438ee33ec753c47718fa63f1407b67
Andrew-Tula <troyka-service-director@yandex.ru>
2023-01-05 15:13:47 +0300

    Добавьте файлы проекта.

---
+++ SimpleTestingStrings/Program.cs
@@ -0,0 +1,13 @@
+﻿
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введите с клавиатуры число");
+        
+        int n = int.Parse(Console.ReadLine());
+        Console.WriteLine(n);
+        Console.ReadLine();
+    }
+}
+    
